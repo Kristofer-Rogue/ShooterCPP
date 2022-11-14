2b89d4399eeae741d3088fa3a923c0ebd1704141
kristof-game <ice99dragon@gmail.com>
2022-11-15 05:39:20 +1000

    Added pickup rotation

--- enc_temp_folder/875b24dacbbecdd09dc1e49d8a33fa7/ShooterBasePickup.cpp
+++ enc_temp_folder/875b24dacbbecdd09dc1e49d8a33fa7/ShooterBasePickup.cpp
@@ -19,7 +19,7 @@ AShooterBasePickup::AShooterBasePickup()
 void AShooterBasePickup::BeginPlay()
 {
 	Super::BeginPlay();
-	GenerateRotationYaw();
+
 	check(CollisionComponent);
 }
 
@@ -37,8 +37,6 @@ void AShooterBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
 void AShooterBasePickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 bool AShooterBasePickup::GivePickupTo(APawn* PlayerPawn)
@@ -59,16 +57,9 @@ void AShooterBasePickup::PickupWasTaken()
 
 void AShooterBasePickup::Respawn()
 {
-	GenerateRotationYaw();
 	CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
 	if (GetRootComponent())
 	{
 		GetRootComponent()->SetVisibility(true, true);
 	}
-}
-
-void AShooterBasePickup::GenerateRotationYaw()
-{
-	const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
-	RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
